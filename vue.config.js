1c1558b6bf2e5abb4ad6916e52ac0c26afa2d39c
Maksim Verkhoturov <sd32@protonmail.com>
2021-02-07 16:44:05 -0700

    adds alias imports for components

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@C': path.resolve(__dirname, 'src/components/')
+      }
+    }
+  }
+}
