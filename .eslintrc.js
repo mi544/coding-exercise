6c69f9d0954067257be4647c4be81549736c50f7
Maksim Verkhoturov <sd32@protonmail.com>
2021-02-07 23:02:34 -0700

    attemts to fix postcss bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
+    'eslint:recommended',
     '@vue/airbnb',
     'prettier',
     'prettier/vue'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
