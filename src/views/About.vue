d58c0b6e8dcf1a0293192184f5c165c68b0ad194
Maksim Verkhoturov <sd32@protonmail.com>
2021-02-08 17:51:21 -0700

    makes About page mobile-responsive

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="about">
-    <article class="bg-blue-50 w-1/2 ml-auto mr-8 mt-4 p-6 shadow-xl">
+    <article
+      class="bg-blue-50 w-3/4 md:w-1/2 ml-auto mr-8 mt-4 mb-12 md:mb-2 p-6 shadow-xl"
+    >
       <img src="../assets/virus2.svg" alt="virus icon" class="max-w-1/3 mx-auto" />
       <p class="text-xl my-4">
         This simple mini-project allows to see inside the pandemic and understand how
         viruses travel and infect everything around them.
       </p>
     </article>
-    <article class="bg-blue-50 w-1/3 mr-auto ml-12 p-6 shadow-lg">
+    <article class="bg-blue-50 w-3/4 md:w-1/3 mr-auto ml-12 p-6 shadow-lg">
       <p class="text-xl">
         With the slick and light design, as well as intricate logic behind the scenes, it
         aims to show how simple and quick an app can be developed in Vue with TailwindCSS.
@@ -18,7 +20,7 @@
         <span class="font-semibold">'Instructions'</span> button.
       </p>
     </article>
-    <article class="bg-blue-50 w-1/2 mx-auto mt-12 p-6 shadow-md">
+    <article class="bg-blue-50 w-3/4 md:w-1/2 mx-auto mt-12 p-6 shadow-md">
       <p class="text-xl">
         This project is built with <span class="font-semibold">Vue.js</span> and
         <span class="font-semibold">TalwindCSS</span>.
